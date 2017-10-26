cbf644628e1c7971d3acdc65c1a33bec9ef2ea95
liconrepo <khairul.alam.licon@framgia.com>
2017-10-26 14:38:34 +0700

    soft ime added

---
+++ app/src/main/java/com/licon/androiddialpadkeyboard/DialpadKeyboard.java
@@ -0,0 +1,90 @@
+package com.licon.androiddialpadkeyboard;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.inputmethodservice.InputMethodService;
+import android.inputmethodservice.Keyboard;
+import android.inputmethodservice.KeyboardView;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
+import android.view.View;
+import android.view.inputmethod.InputConnection;
+
+/**
+ * Created by FRAMGIA\khairul.alam.licon on 25/10/17.
+ */
+public class DialpadKeyboard extends InputMethodService implements KeyboardView.OnKeyboardActionListener {
+    private static final int KEYCODE_FACEBOOK = -20;
+    private static final int KEYCODE_CALL = -21;
+    private static final int KEYCODE_PLUS = -22;
+    private KeyboardView mKeyboardView;
+    private Keyboard mKeyboard;
+
+    @Override
+    public View onCreateInputView() {
+        mKeyboardView = (KeyboardView) getLayoutInflater().inflate(R.layout.layout_keyboard_view, null);
+        mKeyboard = new Keyboard(this, R.xml.qwerty);
+        mKeyboardView.setKeyboard(mKeyboard);
+        mKeyboardView.setOnKeyboardActionListener(this);
+        return mKeyboardView;
+    }
+
+    @Override
+    public void onPress(int primaryCode) {
+    }
+
+    @Override
+    public void onRelease(int primaryCode) {
+    }
+
+    @Override
+    public void onKey(int primaryCode, int[] keyCodes) {
+        InputConnection inputConnection = getCurrentInputConnection();
+        switch (primaryCode) {
+            case Keyboard.KEYCODE_DELETE:
+                inputConnection.deleteSurroundingText(1, 0);
+                break;
+            case KEYCODE_FACEBOOK:
+                Intent intentFacebook = new Intent("android.intent.category.LAUNCHER");
+                intentFacebook.setClassName("com.facebook.katana", "com.facebook.katana.LoginActivity");
+                intentFacebook.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intentFacebook);
+                break;
+            case KEYCODE_CALL:
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) return;
+                Intent intentCall = new Intent(Intent.ACTION_CALL);
+                intentCall.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                String number = "tel:"+inputConnection.getTextBeforeCursor(20, 0).toString();
+                intentCall.setData(Uri.parse(number));
+                startActivity(intentCall);
+                break;
+            case KEYCODE_PLUS:
+                inputConnection.commitText("+", 1);
+                break;
+            default:
+                char code = (char) primaryCode;
+                inputConnection.commitText(String.valueOf(code), 1);
+        }
+    }
+
+    @Override
+    public void onText(CharSequence text) {
+    }
+
+    @Override
+    public void swipeLeft() {
+    }
+
+    @Override
+    public void swipeRight() {
+    }
+
+    @Override
+    public void swipeDown() {
+    }
+
+    @Override
+    public void swipeUp() {
+    }
+}
